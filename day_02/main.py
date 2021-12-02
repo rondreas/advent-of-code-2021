e6fcb7cdbd6c08c86f62e76817a447a2af6ceb07
Andreas Rånman <rondreas@users.noreply.github.com>
2021-12-02 22:25:20 +0100

    Update main.py
    Forgot to use the dataclass decorator, but guess I didn't want that anyway

--- day_02/main.py
+++ day_02/main.py
@@ -7,10 +7,6 @@
 
 """
 
-
-from dataclasses import dataclass
-
-
 __author__ = "Andreas Rånman"
 
 
@@ -44,8 +40,52 @@ class Submarine:
             case _:
                 raise ValueError(f"Failed to parse instruction: {instruction}")
 
+class CorrectSubmarine(Submarine):
+    aim: int = 0
+
+    def move(self, instruction: str):
+        """
+        >>> submarine = CorrectSubmarine()
+        >>> instructions = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+        >>> for instruction in instructions:
+        ...     submarine.move(instruction)
+        >>> submarine.horizontal_position
+        15
+        >>> submarine.depth
+        60
+        """
+        direction, distance = instruction.split()
+        match direction:
+            case "forward":
+                self.horizontal_position += int(distance)
+                self.depth += self.aim * int(distance)
+
+            case "up":
+                self.aim -= int(distance)
+
+            case "down":
+                self.aim += int(distance)
+
+            case _:
+                raise ValueError(f"Failed to parse instruction: {instruction}")
+
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
+    with open("input", "r") as f:
+        instructions = f.readlines()
+
+    submarine = Submarine()
+    correct_submarine = CorrectSubmarine()
+
+    for instruction in instructions:
+        submarine.move(instruction)
+        correct_submarine.move(instruction)
+
+    # What do you get if you multiply your final horizontal position by your final depth?
+    print(submarine.horizontal_position * submarine.depth)
+
+    # What do you get if you multiply your final horizontal position by your final depth?
+    print(correct_submarine.horizontal_position * correct_submarine.depth)
